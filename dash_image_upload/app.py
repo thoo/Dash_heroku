7b0bae45e3a6036e6cd275cba2fdcc5c28e3c1ac
thoo <theinhtay@gmail.com>
2018-10-28 14:47:36 -0400

    update app.py to plot multiple images

--- dash_image_upload/app.py
+++ dash_image_upload/app.py
@@ -5,6 +5,15 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
+import numpy as np
+import openface as opf
+import dlib
+from skimage.color import rgb2gray
+
+import base64
+from skimage import io
+
+dlib_fun=opf.AlignDlib('./lib/dlib_model/shape_predictor_68_face_landmarks.dat')
 my_hr_style={
                 #'border-width' : '4px',
                 'border-top': '2px solid #21ABCD',
@@ -13,6 +22,13 @@ my_hr_style={
                 'margin-left': 'auto',
                 'margin-right': 'auto'
             }
+h5_style = {
+            'textAlign': 'center',
+            'color': 'black',
+            'margin-top': '1.0rem',
+            'margin-bottom': '1.0rem'
+        }
+
 app = dash.Dash()
 
 app.scripts.config.serve_locally = True
@@ -59,26 +75,52 @@ app.layout = html.Div([
     html.Div(id='output-image-upload'),
 ])
 
+def get_face(contents):
+    if contents.startswith('data:image/jpeg;base64,'):
+        contents=contents[len('data:image/jpeg;base64,'):]
+    imgdata = base64.b64decode(contents)
+    arr = np.frombuffer(imgdata,np.uint8)
+    #arr = io.imread(imgdata,plugin='imageio')
+    # arr = base64.decodestring(contents.encode('ascii'))
+    # arr = np.frombuffer(arr, dtype = np.float)
+    return arr
+
+def arrtobase64(arr):
+    return 'data:image/png;base64,{}'.format(base64.encodestring(arr).decode('utf-8'))
+
+def layout_plot(list_images):
+    layout_img = []
+    for image in list_images:
+        layout_img.extend([
+           html.Img(src=image, style={ 'margin' : 'auto', 'display': 'block'}),
+           html.Hr(style=my_hr_style),
+        ])
+    return layout_img
 
 def parse_contents(contents, filename):
-    return html.Div([
-        html.H5(filename, style={
-            'textAlign': 'center',
-            'color': 'black',
-            'margin-top': '1.0rem',
-            'margin-bottom': '1.0rem'
-        }),
-        #html.H6(datetime.datetime.fromtimestamp(date)),
 
+    test = get_face(contents)
+    encoded_img = arrtobase64(test)
+    import pdb;pdb.set_trace()
+    return html.Div([
+        html.Div([
+            html.H5(filename, style= h5_style),
+        #html.H6(datetime.datetime.fromtimestamp(date)),
         # HTML images accept base64 encoded strings in the same format
         # that is supplied by the upload
-        html.Img(src=contents, style = { 'margin' : 'auto', 'display': 'block'}),
-        html.Hr(style=my_hr_style),
+            html.Img(src=contents, style = { 'margin' : 'auto', 'display': 'block'}),
+            html.Hr(style=my_hr_style),
+            ]),
+        html.Div(
+            layout_plot([encoded_img])
+            # html.Img(src=encoded_img, style={ 'margin' : 'auto', 'display': 'block'}),
+            # html.Hr(style=my_hr_style),
         #html.Div('Raw Content'),
         # html.Pre(contents[0:200] + '...', style={
         #     'whiteSpace': 'pre-wrap',
         #     'wordBreak': 'break-all'
         # })
+            )#
     ])
 
 @app.callback(Output('output-image-upload', 'children'),
@@ -97,4 +139,4 @@ app.css.append_css({
 })
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8818)
+    app.run_server(debug=True, port=8819)
